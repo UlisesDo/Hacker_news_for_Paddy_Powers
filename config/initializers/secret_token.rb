700f10af92011d428ec75e16a96b96af1354d491
Ulises Dominguez <ulises_dguez_brito@yahoo.es>
2012-12-07 19:53:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNewsForPaddyPowers::Application.config.secret_token = '8ed29fddf0423f594a4aa2e949ed46a113a98541b130070e96379727455e85e92e747ea14fafb79040fd5c12d6d347cd75faa523f3cdca66f6006156435576a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HackerNewsForPaddyPowers::Application.config.session_store :cookie_store, key: '_Hacker_news_for_Paddy_Powers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HackerNewsForPaddyPowers::Application.config.session_store :active_record_store
